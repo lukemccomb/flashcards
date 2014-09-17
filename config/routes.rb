57b140fec649956c5bf6226ae6eac06408c1171b
Luke McComb <lucasmccomb@gmail.com>
2014-09-16 20:17:42 -0600

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  get "/decks" => "decks#index"
+  get "/decks/new" => "decks#new"
+  get "decks/:id" => "decks#show", as: :deck
+  post "/decks" => "decks#create"
+  get "/decks/:id/edit" => "decks#edit"
+  patch "decks/:id" => "decks#update"
+  delete "decks/:id" => "decks#destroy"
+end
